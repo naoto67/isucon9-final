1f8b510edc77b89b524cb03c9d524fadaf586617
naoto67 <uver.com67@gmail.com>
2020-08-06 09:01:36 +0900

    index追加

--- webapp/sql/01_schema.sql
+++ webapp/sql/01_schema.sql
@@ -79,7 +79,8 @@ CREATE TABLE `train_timetable_master` (
   `train_name` varchar(100) NOT NULL,
   `station` varchar(100) NOT NULL,
   `departure` time NOT NULL,
-  `arrival` time NOT NULL
+  `arrival` time NOT NULL,
+  INDEX primary_idx (`date`, `train_class`, `train_name`, `station`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `users`;
